25c3a693e51f3016452a4b6b0a343d291c2e54a1
amirmosio <amir.mn.mosio@gmail.com>
2020-03-16 04:14:35 +0330

    add volley

--- app/src/main/java/com/example/Service/MapBoxSRV.java
+++ app/src/main/java/com/example/Service/MapBoxSRV.java
@@ -1,39 +1,39 @@
 package com.example.Service;
 
-//import com.android.volley.Request<>;
-//import com.android.volley.Response;
-//import com.android.volley.VolleyError;
-//import com.android.volley.toolbox.JsonObjectRequest;
-//
-//import org.json.JSONArray;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-//import edu.sharif.Weather.R;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.weather_app.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MapBoxSRV {
-//    public static JSONArray getMatchesCity(String preCityName) {
-//        JSONArray[] result = new JSONArray[1];
-//        String url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + preCityName + ".json?access_token=" + R.string.map_box_token;
-//
-//        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        try {
-//                            result[0] = response.getJSONArray("features");
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        result[0] = null;
-//                    }
-//                });
-//        return result[0];
-//
-//    }
+    public static JSONArray getMatchesCity(String preCityName) {
+        final JSONArray[] result = new JSONArray[1];
+        String url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + preCityName + ".json?access_token=" + R.string.map_box_token;
+
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            result[0] = response.getJSONArray("features");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        result[0] = null;
+                    }
+                });
+        return result[0];
+
+    }
 }
